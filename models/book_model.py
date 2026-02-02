8ae1a6f67533ce43f7c0a9f3e5da39b44bd0f437
Shreyansh Jain <shreyanshwork@gmail.com>
2026-02-02 15:31:45 +0530

    Update BookRequest Model with model_config

--- models/book_model.py
+++ models/book_model.py
@@ -2,8 +2,19 @@ from pydantic import BaseModel, Field
 from typing import Optional
 
 class BookRequest(BaseModel):
-    id: Optional[int] = None
+    id: Optional[int] = Field(description="ID is not needed for book creation", default=None)
     title: str = Field(min_length=3)
     author: str = Field(min_length=1)
     description:str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, le=5)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "A new Book",
+                "author": "Name of the Author",
+                "description": "Summary ro description about the book.",
+                "rating": 5
+            }
+        }
+    }
